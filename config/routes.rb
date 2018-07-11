2bba37bafeedadc45bf4b1e9067fd33f2f210927
Kimeunsoo <rmsid1312@likelion.org>
2018-07-11 10:55:01 +0000

    위대한 성공

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
   get 'posts/index'
-# asdasdasdasdasdasdasdasd
+# 성공
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
